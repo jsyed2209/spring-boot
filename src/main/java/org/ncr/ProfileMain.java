14212f5dc40c27d713ea80e5a64259e97e9a6c42
jsyed2209 <45897532+jsyed2209@users.noreply.github.com>
2024-02-23 12:35:02 +0530

    Create ProfileMain.java

---
+++ src/main/java/org/ncr/ProfileMain.java
@@ -0,0 +1,13 @@
+// src/main/java/com/example/demo/SpringBootIsAliveApplication.java
+package org.ncr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileMain.class, args);
+    }
+}
